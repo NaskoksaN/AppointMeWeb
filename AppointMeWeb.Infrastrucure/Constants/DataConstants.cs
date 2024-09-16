47a62bb48d89708a0c52ac737bb8dddf7352d0da
NaskoksaN <nbobchev@yahoo.com>
2024-09-16 13:38:30 +0300

    add more fields in ConstandMessage and Data

--- AppointMeWeb.Infrastrucure/Constants/DataConstants.cs
+++ AppointMeWeb.Infrastrucure/Constants/DataConstants.cs
@@ -3,14 +3,27 @@
     public static class DataConstants
     {
         public const int ApplicationUserFirstNameMaxLength = 30;
+        public const int ApplicationUserFirstNameMinLength = 1;
+
         public const int ApplicationUserLastNameMaxLength = 30;
+        public const int ApplicationUserLastNameMinLength = 1;
+
         public const int UserPhoneMaxLength = 20;
+        public const int UserPhoneMinLength = 6;
+
         public const int AppointmentMessageMaxLength = 150;
+
         public const int BusinessServiceProviderNameMaxLength = 50;
+
+
         public const int BusinessServiceProviderDescriptionMaxLength = 50;
+
         public const int BusinessServiceProviderEmailMaxLength = 50;
+
         public const int BusinessServiceProviderTownMaxLength = 50;
+
         public const int BusinessServiceProviderAddressMaxLength = 50;
+
         public const int BusinessServiceProviderUrlMaxLength = 250;
     }
 }
