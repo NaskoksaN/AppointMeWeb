285f7bd57ee6c7e92a17e5fe747435733ce8f382
NaskoksaN <nbobchev@yahoo.com>
2024-09-16 11:24:29 +0300

    Add roles in Db

--- AppointMeWeb.Infrastrucure/Data/Models/ApplicationUser.cs
+++ AppointMeWeb.Infrastrucure/Data/Models/ApplicationUser.cs
@@ -27,7 +27,7 @@ namespace AppointMeWeb.Infrastrucure.Data.Models
 
         [Required]
         [Comment("User date of birth")]
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; } 
         
         [Comment("Associated business service provider")]
         public int? BusinessServiceProviderId { get; set; }
