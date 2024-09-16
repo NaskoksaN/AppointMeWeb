589f18a8c35488b26abcbbb95ebdedd4daa00136
NaskoksaN <nbobchev@yahoo.com>
2024-09-16 13:39:10 +0300

    add httpget Register in controller

--- AppointMeWeb/Controllers/UserController.cs
+++ AppointMeWeb/Controllers/UserController.cs
@@ -1,6 +1,10 @@
-﻿using AppointMeWeb.Infrastrucure.Data.Models;
+﻿
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+
+using AppointMeWeb.Infrastrucure.Data.Models;
+using AppointMeWeb.Core.Contracts;
+using AppointMeWeb.Core.Models.ApplicationUser;
 
 namespace AppointMeWeb.Controllers
 {
@@ -8,19 +12,27 @@ namespace AppointMeWeb.Controllers
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ICustomUserService customUserService;
 
         public UserController(UserManager<ApplicationUser> _userManager
-                , SignInManager<ApplicationUser> _signInManager)
+                , SignInManager<ApplicationUser> _signInManager
+                ,ICustomUserService _customUserService )
         {
             this.userManager = _userManager;
             this.signInManager = _signInManager;
+            this.customUserService = _customUserService;
         }
 
         [HttpGet]
         public async Task<IActionResult> Register()
         {
+            IEnumerable<RoleViewModel> roles = await customUserService.GetRolesAsync();
+            RegisterFormModel formModel = new ()
+            {
+                Roles = roles
+            };
 
-            return View();
+            return View(formModel);
         }
     }
 }
