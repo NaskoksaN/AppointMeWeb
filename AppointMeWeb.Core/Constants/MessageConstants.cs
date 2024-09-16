67fbdff8caf272aa1e66dd4ebfbe1c0b454b4ee8
NaskoksaN <nbobchev@yahoo.com>
2024-09-16 13:38:30 +0300

    add more fields in ConstandMessage and Data

---
+++ AppointMeWeb.Core/Constants/MessageConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointMeWeb.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string RequiredMessage = "The {0} is required.";
+        public const string LengthMessage = "The {0} must be between {2} and {1} characters long.";
+    }
+}
