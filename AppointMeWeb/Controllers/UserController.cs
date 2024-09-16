9fe6bec4031cf53b8f0eddb9cc1e97fc0bb800db
NaskoksaN <nbobchev@yahoo.com>
2024-09-16 09:57:33 +0300

    Add Base controller

---
+++ AppointMeWeb/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using AppointMeWeb.Infrastrucure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointMeWeb.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UserController(UserManager<ApplicationUser> _userManager
+                , SignInManager<ApplicationUser> _signInManager)
+        {
+            this.userManager = _userManager;
+            this.signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+
+            return View();
+        }
+    }
+}
