471794f799a8eaebdaa62646e0969174ba0dd2f6
NaskoksaN <nbobchev@yahoo.com>
2024-09-16 13:38:48 +0300

    Seed rolse in Db

--- AppointMeWeb.Infrastrucure/Data/ApplicationDbContext.cs
+++ AppointMeWeb.Infrastrucure/Data/ApplicationDbContext.cs
@@ -4,16 +4,39 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 using AppointMeWeb.Infrastrucure.Data.Models;
+using AppointMeWeb.Infrastrucure.Data.Configuration;
 
 namespace AppointMeWeb.Infrastrucure.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<string>, string>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        private readonly bool seedDb;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, bool _seedDb=true)
             : base(options)
         {
-        }
+            if (Database.IsRelational())
+            {
+                Database.Migrate();
+            }
+            else
+            {
+                Database.EnsureCreated();
+            }
 
+            seedDb = _seedDb;
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            if (seedDb)
+            {
+                builder.ApplyConfiguration(new IdentityRoleConfiguration());
+            }
+            
+
+            base.OnModelCreating(builder);
+        }
         public DbSet<BusinessServiceProvider> BusinessServiceProviders { get; set; }
         public DbSet<WorkingSchedule> WorkingSchedules { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
