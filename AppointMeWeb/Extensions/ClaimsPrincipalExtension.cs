8de934de4a7883bea3a38edfbe2e00ccb44dcc14
NaskoksaN <nbobchev@yahoo.com>
2024-09-19 12:27:44 +0300

    Add claims for Admin, User and Business

--- AppointMeWeb/Extensions/ClaimsPrincipalExtension.cs
+++ AppointMeWeb/Extensions/ClaimsPrincipalExtension.cs
@@ -1,11 +1,31 @@
 ﻿using System.Security.Claims;
 
+using static AppointMeWeb.Infrastrucure.Constants.DataConstants;
+
 namespace AppointMeWeb.Extensions
 {
     public static class ClaimsPrincipalExtension
     {
-        public static string Id (this ClaimsPrincipal user)
-                    => user.FindFirstValue(ClaimTypes.NameIdentifier);
-        
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRole);
+        }
+
+        public static bool IsUser(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(WebUserRole);
+        }
+
+        public static bool IsBusinessProvider(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(BusinessRole);
+        }
+
+
     }
 }
