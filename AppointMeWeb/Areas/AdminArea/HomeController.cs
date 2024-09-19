38ecce76ccf846f38df638882084ff49afa18f7a
NaskoksaN <nbobchev@yahoo.com>
2024-09-19 12:25:50 +0300

    Create Admin, User and Business Area

---
+++ AppointMeWeb/Areas/AdminArea/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppointMeWeb.Areas.AdminArea
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult AdminHomeIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppointMeWeb/Areas/BusinessArea/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using static AppointMeWeb.Infrastrucure.Constants.DataConstants;
+
+namespace AppointMeWeb.Areas.BusinessArea
+{
+    [Area("AdminArea")]
+    [Authorize(Roles = BusinessRole)]
+    public class BaseController : Controller
+    {
+
+    }
+}
