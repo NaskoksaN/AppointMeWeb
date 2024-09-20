99950360e8699f6e3e9420e6adb7e8bc6f090f43
NaskoksaN <nbobchev@yahoo.com>
2024-09-20 16:11:29 +0300

    Add components

---
+++ AppointMeWeb/Components/BusinessAreaMenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppointMeWeb.Components
+{
+    public class BusinessAreaMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
--- AppointMeWeb/Components/MainMenuComponent.cs
+++ AppointMeWeb/Components/MainMenuComponent.cs
@@ -6,10 +6,10 @@ namespace AppointMeWeb.Components
     public class MainMenuComponent : ViewComponent
     {
        public async Task<IViewComponentResult> InvokeAsync()
-        {
-
-
+       {
             return await Task.FromResult<IViewComponentResult>(View());
-        }
+       }
     }
+
+    
 }
