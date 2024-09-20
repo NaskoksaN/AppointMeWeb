14c37ed03168e4b884dfcfba4f7b05f9cc18bd0e
NaskoksaN <nbobchev@yahoo.com>
2024-09-20 19:08:02 +0300

    refactor , add summary and try catch

--- AppointMeWeb.Core/Contracts/ICustomUserService.cs
+++ AppointMeWeb.Core/Contracts/ICustomUserService.cs
@@ -11,10 +11,54 @@ namespace AppointMeWeb.Core.Contracts
 {
     public interface ICustomUserService
     {
+        /// <summary>
+        /// Asynchronously retrieves a list of roles from the role manager.
+        /// </summary>
+        /// <returns>
+        /// A task that represents the asynchronous operation. 
+        /// The task result contains an IEnumerable of RoleViewModel, which includes the role Id and Name.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// Thrown when an error occurs while fetching roles from the role manager.
+        /// The inner exception provides more details on the error.
+        /// </exception>
         Task<IEnumerable<RoleViewModel>> GetRolesAsync();
+
+        /// <summary>
+        /// Asynchronously retrieves the roles associated with the specified user.
+        /// </summary>
+        /// <param name="user">The user for whom to retrieve roles.</param>
+        /// <returns>A list of role names associated with the user.</returns>
+        /// <exception cref="ApplicationException">Thrown when an error occurs while fetching roles.</exception>
         Task<IList<string>> GetUserRoleAsync(ApplicationUser user);
-        Task<ApplicationUser> LoginUserAsync(LoginFormModel model);
+
+        /// <summary>
+        /// Asynchronously logs in a user using the provided login model.
+        /// </summary>
+        /// <param name="model">The login model containing email and password.</param>
+        /// <returns>The logged-in user if successful; otherwise, null.</returns>
+        Task<ApplicationUser?> LoginUserAsync(LoginFormModel model);
+
+        /// <summary>
+        /// Registers a new user in the system based on the provided model, assigns the appropriate role,
+        /// and if the user is a business provider, associates the user with a business entity.
+        /// </summary>
+        /// <param name="model">The registration form data containing user details.</param>
+        /// <returns>A task representing the asynchronous operation. The task result is true if the registration 
+        /// was successful, otherwise false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the registration model or the role is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when a user with the provided email already exists.</exception>
+        /// <exception cref="ApplicationException">Thrown when the database operation fails while saving the business ID.</exception>
         Task<bool> RegisterUserAsync(RegisterFormModel model);
+
+        /// <summary>
+        /// Updates the appointment duration details for a specified business user.
+        /// </summary>
+        /// <param name="appointmentDuration">The new appointment duration as a TimeSpan.</param>
+        /// <param name="userId">The ID of the business user whose details are to be updated.</param>
+        /// <returns>The ID of the updated business user.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the business user is not found.</exception>
+        /// <exception cref="ApplicationException">Thrown when the database operation fails.</exception>
         Task<int> UpdateBusinessUserDurationDetails(TimeSpan appointmentDuration, string userId);
     }
 }
--- AppointMeWeb.Core/Services/Factory.cs
+++ AppointMeWeb.Core/Services/Factory.cs
@@ -3,6 +3,7 @@ using AppointMeWeb.Core.Models.ApplicationUser;
 using AppointMeWeb.Core.Models.BusinessProvider;
 using AppointMeWeb.Infrastrucure.Data.Common;
 using AppointMeWeb.Infrastrucure.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace AppointMeWeb.Core.Services
@@ -10,15 +11,38 @@ namespace AppointMeWeb.Core.Services
     public class Factory : IFactory
     {
         private readonly IRepository sqlRepository;
-        private readonly ILogger logger;
 
         public Factory(IRepository _sqlRepository
-            , ILogger<IFactory> _logger)
+            )
         {
             this.sqlRepository = _sqlRepository;
-            this.logger = _logger;
         }
 
+        /// <summary>
+        /// Creates a new instance of <see cref="ApplicationUser"/> based on the provided registration form model.
+        /// </summary>
+        /// <param name="model">The registration form model containing user details.</param>
+        /// <returns>A new <see cref="ApplicationUser"/> instance populated with the specified details.</returns>
+        public ApplicationUser CreateApplicationUser( RegisterFormModel model)
+        {
+            return new ApplicationUser
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                DateOfBirth = model.DOB,
+                PhoneNumber = model.PhoneNumber,
+                Email = model.Email,
+                UserName = model.Email,
+            };
+        }
+
+        /// <summary>
+        /// Creates a new business user based on the provided registration details and associates it with the specified user ID.
+        /// </summary>
+        /// <param name="model">The registration form model containing the business details.</param>
+        /// <param name="userId">The ID of the user associated with the business.</param>
+        /// <returns>The ID of the newly created <see cref="BusinessServiceProvider"/>.</returns>
+        /// <exception cref="ApplicationException">Thrown when the database fails to save the information or an unexpected error occurs.</exception>
         public async Task<int> CreateBusinessUserAndReturnId(RegisterFormModel model, string userId)
         {
             
@@ -38,14 +62,24 @@ namespace AppointMeWeb.Core.Services
                 await sqlRepository.SaveChangesAsync();
                 return businessProvider.Id;
             }
+            catch (DbUpdateException dbEx)
+            {
+                throw new ApplicationException("Database failed to save info", dbEx);
+            }
             catch (Exception ex)
             {
-                logger.LogError(nameof(CreateBusinessUserAndReturnId), ex);
-                throw new ApplicationException("Database failed to save info", ex);
+                throw new ApplicationException("An unexpected error occurred.", ex);
             }
 
         }
 
+        /// <summary>
+        /// Creates a work schedule for a business user based on the provided daily schedules.
+        /// </summary>
+        /// <param name="dailySchedules">A list of daily schedules containing details such as day, start time, end time, and whether it's a day off.</param>
+        /// <param name="businessUserId">The identifier of the business user for whom the schedule is created.</param>
+        /// <returns>A task that represents the asynchronous operation, with a value indicating whether the schedule was successfully created.</returns>
+        /// <exception cref="ApplicationException">Thrown when there is an error saving the schedule to the database.</exception>
         public async Task<bool> CreateWorkSchedule(List<DailyScheduleViewModel> dailySchedules, int businessUserId)
         {
             try
