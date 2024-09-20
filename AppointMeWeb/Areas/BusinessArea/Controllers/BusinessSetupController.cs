d16e2339e4095b8b7f735c8b393c3bbfcaeaf916
NaskoksaN <nbobchev@yahoo.com>
2024-09-20 16:08:46 +0300

    Add controller and setup action

---
+++ AppointMeWeb/Areas/BusinessArea/Controllers/BusinessSetupController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using AppointMeWeb.Core.Models.BusinessProvider;
+using AppointMeWeb.Core.Contracts;
+using AppointMeWeb.Extensions;
+using System.Security.Claims;
+
+namespace AppointMeWeb.Areas.BusinessArea.Controllers
+{
+    public class BusinessSetupController : BaseController
+    {
+        private readonly IBusinessService businessService;
+        private readonly ICustomUserService customUserService;
+        private readonly IFactory factory;
+
+        public BusinessSetupController(IBusinessService _businessService
+                , ICustomUserService _customUserService
+                , IFactory _factory)
+        {
+            this.businessService = _businessService;
+            this.customUserService = _customUserService;
+            this.factory = _factory;
+        }
+
+        [HttpGet]
+        public IActionResult Setup()
+        {
+            BusinessProviderFormModel model = new()
+            {
+                Days = Enum.GetValues(typeof(DayOfWeek))
+                   .Cast<DayOfWeek>()
+                   .Skip(1)
+                   .Concat(new[] { DayOfWeek.Sunday })
+                   .ToList()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Setup([FromForm] BusinessProviderFormModel model)
+        {
+            try
+            {
+                string userId = User.Id();
+                int businessUserId = await customUserService
+                            .UpdateBusinessUserDurationDetails(model.AppointmentDuration, userId);
+                bool isWorkScheduleUpdate = await factory
+                            .CreateWorkSchedule(model.DailySchedules, businessUserId);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                BusinessProviderFormModel modelView = new()
+                {
+                    Days = Enum.GetValues(typeof(DayOfWeek))
+                  .Cast<DayOfWeek>()
+                  .Skip(1)
+                  .Concat(new[] { DayOfWeek.Sunday })
+                  .ToList()
+                };
+                return View(modelView);
+            }
+
+            
+        }
+    }
+}
