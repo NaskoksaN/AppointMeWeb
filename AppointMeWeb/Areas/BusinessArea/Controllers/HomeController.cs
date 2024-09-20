59935a8c4b7fb2158cef1b82accdf3dd14680fe1
NaskoksaN <nbobchev@yahoo.com>
2024-09-20 08:50:34 +0300

    Create Business area with _Layout and Index view

--- AppointMeWeb/Areas/BusinessArea/Controllers/HomeController.cs
+++ AppointMeWeb/Areas/BusinessArea/Controllers/HomeController.cs
@@ -1,11 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppointMeWeb.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Razor;
 
 namespace AppointMeWeb.Areas.BusinessArea.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        private readonly IActionContextAccessor _actionContextAccessor;
+        private readonly IRazorViewEngine _razorViewEngine;
+
+        public HomeController(IActionContextAccessor actionContextAccessor, IRazorViewEngine razorViewEngine)
+        {
+            _actionContextAccessor = actionContextAccessor;
+            _razorViewEngine = razorViewEngine;
+        }
+
         public IActionResult BusinessHomeIndex()
         {
+            //    var context = _actionContextAccessor.ActionContext;
+            //    //var viewResult = _razorViewEngine.GetView(executingFilePath: null, viewPath: "BusinessHomeIndex", isMainPage: false);
+            //    var viewResult = _razorViewEngine.GetView(
+            //executingFilePath: null,
+            //viewPath: "/Areas/BusinessArea/Views/Home/BusinessHomeIndex.cshtml",
+            //isMainPage: false);
+
+            //    if (!viewResult.Success)
+            //    {
+            //        // Log or debug view locations
+            //        var searchedLocations = viewResult.SearchedLocations;
+            //        foreach (var location in searchedLocations)
+            //        {
+            //            Console.WriteLine($"Searched location: {location}");
+            //        }
+
+            //        return NotFound();
+            //    }
+
             return View();
         }
     }
