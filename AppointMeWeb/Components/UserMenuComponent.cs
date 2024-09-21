3c6d18544c1aa51f36940b0f749d9681d8986235
NaskoksaN <nbobchev@yahoo.com>
2024-09-21 08:58:38 +0300

    Create USerMenuComponent

---
+++ AppointMeWeb/Components/UserMenuComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AppointMeWeb.Components
+{
+    public class UserMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
