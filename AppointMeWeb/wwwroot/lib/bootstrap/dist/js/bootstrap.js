d2b71d32a8188736bf1f822a466ee867876669c1
NaskoksaN <nbobchev@yahoo.com>
2024-09-22 08:36:46 +0300

    Fix depricated issues

--- AppointMeWeb/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ AppointMeWeb/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -593,8 +593,7 @@
       }
 
       if (isNative) {
-        evt = document.createEvent('HTMLEvents');
-        evt.initEvent(typeEvent, bubbles, true);
+        evt = new Event(typeEvent, { bubbles: bubbles, cancelable: true });
       } else {
         evt = new CustomEvent(event, {
           bubbles,
@@ -997,8 +996,8 @@
     offset(element) {
       const rect = element.getBoundingClientRect();
       return {
-        top: rect.top + window.pageYOffset,
-        left: rect.left + window.pageXOffset
+        top: rect.top + window.scrollY,
+        left: rect.left + window.scrollX
       };
     },
 
