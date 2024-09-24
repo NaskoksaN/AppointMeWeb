207583ecd5fdce2e9070b8aa3bdd5161e92118f9
NaskoksaN <nbobchev@yahoo.com>
2024-09-24 12:44:14 +0300

    Add ExistSchedule in Model, where after creating user can edit his working time

---
+++ AppointMeWeb.Core/Models/BusinessProvider/BusinessViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppointMeWeb.Core.Models.BusinessProvider
+{
+    public class BusinessViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+       
+        public string Name { get; set; } = string.Empty;
+      
+        public string BusinessType {  get; set; } = string.Empty;
+        
+        public string Description { get; set; } = string.Empty;
+        
+        public string Phone { get; set; } = string.Empty;
+      
+        public string Email { get; set; } = string.Empty;
+       
+        public string Town { get; set; } = string.Empty;
+    
+        public string Address { get; set; } = string.Empty;
+        public string WebsiteUrl { get; set; } = string.Empty; 
+
+    }
+}
