dc72fdb66691ed0dfea8f6dc6d0a7ea18045bc4b
NaskoksaN <nbobchev@yahoo.com>
2024-09-24 19:01:04 +0300

    Create AppointMeViewModels

---
+++ AppointMeWeb.Core/Models/AppointmeModels/AppointmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AppointMeWeb.Core.Models.BusinessProvider;
+
+namespace AppointMeWeb.Core.Models.AppointmeModels
+{
+    public class AppointmentViewModel
+    {
+        public int Id { get; set; }
+        public List<DateTime> NextThirtyDays { get; set; } = [];
+        public List<DailyScheduleViewModel> BusinessWorkingSchedule { get; set; } = [];
+        public List<AppointmentSlotViewModel> AvailableSlots { get; set; } = [];
+    }
+
+}
+
