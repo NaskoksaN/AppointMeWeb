02e660620a090d7973fbd2ab47f008cf693f15bd
NaskoksaN <nbobchev@yahoo.com>
2024-09-24 18:58:37 +0300

    Fix bug - password length registration

--- AppointMeWeb.Core/Models/ApplicationUser/LoginFormModel.cs
+++ AppointMeWeb.Core/Models/ApplicationUser/LoginFormModel.cs
@@ -7,10 +7,13 @@ namespace AppointMeWeb.Core.Models.ApplicationUser
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; } = string.Empty;
 
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; } = null!;
 
         public string? ReturnUrl { get; set; }
