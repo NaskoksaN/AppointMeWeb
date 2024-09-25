75563427c8221727339d67638c4cc12bd2dcd316
NaskoksaN <nbobchev@yahoo.com>
2024-09-25 10:19:01 +0300

    Add ServiceInfo, where list keep businesstype and url

---
+++ AppointMeWeb.Core/Models/HomeModels/ServiceInfoModels.cs
@@ -0,0 +1,15 @@
+﻿using AppointMeWeb.Infrastrucure.Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointMeWeb.Core.Models.HomeModels
+{
+    public class ServiceInfoModels
+    {
+        public BusinessType BusinessType { get; set; }
+        public string ImageUrl { get; set; } = null!;
+    }
+}
--- AppointMeWeb.Core/Services/BusinessService.cs
+++ AppointMeWeb.Core/Services/BusinessService.cs
@@ -1,6 +1,8 @@
 ﻿using AppointMeWeb.Core.Contracts;
 using AppointMeWeb.Core.Models.BusinessProvider;
+using AppointMeWeb.Core.Models.HomeModels;
 using AppointMeWeb.Infrastrucure.Data.Common;
+using AppointMeWeb.Infrastrucure.Data.Enum;
 using AppointMeWeb.Infrastrucure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,11 +11,34 @@ namespace AppointMeWeb.Core.Services
     public class BusinessService : IBusinessService
     {
         private readonly IRepository sqlService;
-        public BusinessService( IRepository _sqlService)
-        {
+        private readonly List<ServiceInfoModels> businessInfo = new List<ServiceInfoModels>
+            {
+                new ServiceInfoModels { BusinessType = BusinessType.Doctor, ImageUrl = "https://plus.unsplash.com/premium_photo-1673953510197-0950d951c6d9?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Dentist, ImageUrl = "https://plus.unsplash.com/premium_photo-1675686363507-22a8d0e11b4c?q=80&w=1806&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Barber, ImageUrl = "https://images.unsplash.com/photo-1599351431613-18ef1fdd27e1?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Hairdresser, ImageUrl = "https://plus.unsplash.com/premium_photo-1669675935483-01a22e5c88bf?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Manicurist, ImageUrl = "https://images.unsplash.com/photo-1602585578130-c9076e09330d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Lawyer, ImageUrl = "https://plus.unsplash.com/premium_photo-1661329930662-19a43503782f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.CarService, ImageUrl = "https://images.unsplash.com/photo-1625047509248-ec889cbff17f?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Plumber, ImageUrl = "https://plus.unsplash.com/premium_photo-1663045495725-89f23b57cfc5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Electrician, ImageUrl = "https://plus.unsplash.com/premium_photo-1661908782924-de673a5c6988?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Mechanic, ImageUrl = "https://plus.unsplash.com/premium_photo-1677009541474-1fc2642943c1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Accountant, ImageUrl = "https://plus.unsplash.com/premium_photo-1678567671940-64eeefe22e5b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Therapist, ImageUrl = "https://plus.unsplash.com/premium_photo-1663050739359-a4261779f6ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.PersonalTrainer, ImageUrl = "https://plus.unsplash.com/premium_photo-1672862927344-05ba0faa549b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Teacher, ImageUrl = "https://plus.unsplash.com/premium_photo-1682888442432-a1bc427c0d91?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Photographer, ImageUrl = "https://plus.unsplash.com/premium_photo-1682098354910-6823d8fb2ac2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Cleaner, ImageUrl = "https://images.unsplash.com/photo-1527515637462-cff94eecc1ac?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.Chef, ImageUrl = "https://images.unsplash.com/photo-1488992783499-418eb1f62d08?q=80&w=1889&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+                new ServiceInfoModels { BusinessType = BusinessType.MassageTherapist, ImageUrl = "https://plus.unsplash.com/premium_photo-1661266905734-22fb75b75ada?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+              };
 
+        public BusinessService(IRepository _sqlService)
+        {
             this.sqlService = _sqlService;
         }
+
+        public List<ServiceInfoModels> BusinessInfo() => businessInfo;
 
         /// <summary>
         /// Asynchronously retrieves all business service provider records from the database,
@@ -60,7 +85,7 @@ namespace AppointMeWeb.Core.Services
         /// or null if no matching provider is found.
         /// </returns>
         /// <exception cref="ArgumentException">Thrown when the user ID type is unsupported.</exception>
-        public async Task<List<DailyScheduleViewModel>>? GetUserWorkingShedulesAsync<T>(T userId) 
+        public async Task<List<DailyScheduleViewModel>>? GetUserWorkingShedulesAsync<T>(T userId)
         {
 
             var query = sqlService.AllReadOnly<BusinessServiceProvider>();
@@ -89,6 +114,6 @@ namespace AppointMeWeb.Core.Services
             return schedule?.ExistedSchedule;
         }
 
-       
+        
     }
 }
