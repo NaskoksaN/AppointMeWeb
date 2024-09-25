36195896a44fbdf00dabdb89e41dbc9dcba66c23
NaskoksaN <nbobchev@yahoo.com>
2024-09-25 16:09:22 +0300

    Update ViewMoels with more properties

--- AppointMeWeb.Core/Models/AppointmeModels/AppointmentSlotViewModel.cs
+++ AppointMeWeb.Core/Models/AppointmeModels/AppointmentSlotViewModel.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppointMeWeb.Core.Models.AppointmeModels
+﻿namespace AppointMeWeb.Core.Models.AppointmeModels
 {
     public class AppointmentSlotViewModel
     {
-        public DateTime Date { get; set; }
+        public DateOnly Date { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
         public bool IsBooked { get; set; }
--- AppointMeWeb.Core/Models/AppointmeModels/AppointmentViewModel.cs
+++ AppointMeWeb.Core/Models/AppointmeModels/AppointmentViewModel.cs
@@ -5,9 +5,14 @@ namespace AppointMeWeb.Core.Models.AppointmeModels
     public class AppointmentViewModel
     {
         public int Id { get; set; }
-        public List<DateTime> NextThirtyDays { get; set; } = [];
+
+        public List<DateOnly> NextThirtyDays { get; set; } = [];
         public List<DailyScheduleViewModel> BusinessWorkingSchedule { get; set; } = [];
-        public List<AppointmentSlotViewModel> AvailableSlots { get; set; } = [];
+        public string AvaibleSlots {  get; set; } =string.Empty;
+        public Dictionary<DateOnly, string> WorkingHours { get; set; } = [];
+        public Dictionary<DateOnly, string> TooltipTexts { get; set; } = [];
+
+        public Dictionary<DateOnly, List<AppointmentSlotViewModel>> AvailableSlots { get; set; } = [];
     }
 
 }
