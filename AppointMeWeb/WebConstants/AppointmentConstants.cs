af6a05ed1745bf9cd5c1ace0fbbc9111e156e773
NaskoksaN <nbobchev@yahoo.com>
2024-09-30 11:06:55 +0300

    add constant for data, time

--- AppointMeWeb/WebConstants/AppointmentConstants.cs
+++ AppointMeWeb/WebConstants/AppointmentConstants.cs
@@ -1,7 +1,13 @@
-﻿namespace AppointMeWeb.WebConstants
+﻿using Microsoft.VisualBasic;
+
+namespace AppointMeWeb.WebConstants
 {
     public static class AppointmentConstants
     {
         public const int CountOfDays = 30;
+
+        public const string TimeFormat = @"hh\:mm";
+        public const string DateFormat = "yyyy-MM-dd";
+        public const string DayDateFormat = "dddd, MMM dd yyyy";
     }
 }
