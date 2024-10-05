2f03c14768990f7a9c93d30c3398790eb87f3cc9
NaskoksaN <nbobchev@yahoo.com>
2024-10-05 14:56:04 +0300

    Add view models for User to see their Appointments

---
+++ AppointMeWeb.Core/Models/HomeModels/UserHomeIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AppointMeWeb.Infrastrucure.Data.Enum;
+
+namespace AppointMeWeb.Core.Models.HomeModels
+{
+    public class UserHomeIndexViewModel
+    {
+        public int AppointmentId {  get; set; }
+        public int BusinessProviderId {  get; set; }
+        public string BusinessProviderName { get; set; }=string.Empty;
+        public DateOnly AppointmentDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public AppointmentStatus Status {  get; set; }
+    }
+}
