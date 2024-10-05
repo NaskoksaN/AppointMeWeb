153634dcec32e7f8ff66879ae9be4a513cb0813b
NaskoksaN <nbobchev@yahoo.com>
2024-10-05 14:56:04 +0300

    Add view models for User to see their Appointments

---
+++ AppointMeWeb.Core/Models/HomeModels/UserHomeIndexView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointMeWeb.Core.Models.HomeModels
+{
+    public class UserHomeIndexView
+    {
+        public IEnumerable<UserHomeIndexViewModel> Active { get; set; } = [];
+        public IEnumerable<UserHomeIndexViewModel> Canceled { get; set; } = [];
+
+    }
+}
