f18fc1a282fa0ffdc2fb327252468d93b7150ab9
NaskoksaN <nbobchev@yahoo.com>
2024-10-09 20:50:08 +0300

    make models for ClientRecods

---
+++ AppointMeWeb.Core/Models/ClientRecordsModels/ClientRecordFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static AppointMeWeb.Core.Constants.MessageConstants;
+using static AppointMeWeb.Core.Constants.CoreConstants;
+
+namespace AppointMeWeb.Core.Models.ClientRecordsModels
+{
+    public class ClientRecordFormModel
+    {
+        [Required(ErrorMessage = RequiredMessage)]
+        //[EmailAddress(ErrorMessage = EmailErrMsg)]
+        [RegularExpression(RegexEmailValidation, ErrorMessage = EmailErrMsg)]
+        public string SearchByEmail{ get; set; } =string.Empty;
+        
+        public string UserId {  get; set; }=string.Empty;
+    }
+}
---
+++ AppointMeWeb.Core/Models/ClientRecordsModels/ClientRecordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointMeWeb.Core.Models.ClientRecordsModels
+{
+    public class ClientRecordViewModel
+    {
+        public int AppointmentId {  get; set; }
+        public string FullName {  get; set; }=string.Empty;
+        public string Email {  get; set; }=string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string ReasonOfAppointment {  get; set; } = string.Empty;
+        public DateOnly DateOfAppointment { get; set; }
+    }
+}
