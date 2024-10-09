230fc84e6eac1e9ff38726b7e585a0116a341b77
NaskoksaN <nbobchev@yahoo.com>
2024-10-09 20:52:02 +0300

    add regex for mail in Constants

---
+++ AppointMeWeb.Core/Constants/DateConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace AppointMeWeb.Core.Constants
+{
+    public static class DateConstants
+    {
+        public static readonly DateOnly Today = DateOnly.FromDateTime(DateTime.Now);
+        public static readonly DateOnly Tomorrow = DateOnly.FromDateTime(DateTime.Now.AddDays(1));
+        public static readonly DateOnly NextWeekEnd = DateOnly.FromDateTime(DateTime.Now.AddDays(7));
+        public static readonly DateOnly NextThirtyDays = Tomorrow.AddDays(30);
+
+    }
+}
