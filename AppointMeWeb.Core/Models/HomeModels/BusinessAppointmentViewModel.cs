a91b3d29d34934182154ed97eca14bf5210191bc
NaskoksaN <nbobchev@yahoo.com>
2024-10-09 20:50:56 +0300

    refacotr HomeModels. remove view...must decide what to display

---
+++ AppointMeWeb.Core/Models/HomeModels/BusinessAppointmentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AppointMeWeb.Infrastrucure.Data.Enum;
+
+namespace AppointMeWeb.Core.Models.HomeModels
+{
+    public class BusinessAppointmentViewModel
+    {
+        public int AppointmentId { get; set; }
+        public string UserId { get; set; } =string.Empty;
+        public string UserNames { get; set; }=string.Empty;
+        public string UserPhone { get; set; } = string.Empty;
+        public string? UserMessage { get; set; } 
+        public string UserEmail { get; set; } = string.Empty;
+        public DateOnly AppointmentDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public AppointmentStatus Status { get; set; }
+    }
+}
