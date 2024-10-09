3fe37bfcc8f63831cffabdeeae5b69755a5b054f
NaskoksaN <nbobchev@yahoo.com>
2024-10-09 20:50:56 +0300

    refacotr HomeModels. remove view...must decide what to display

---
+++ AppointMeWeb.Core/Models/HomeModels/BusinessHomeIndexView.cs
@@ -0,0 +1,8 @@
+﻿namespace AppointMeWeb.Core.Models.HomeModels
+{
+    public class BusinessHomeIndexView
+    {
+        public IEnumerable<BusinessAppointmentViewModel> Active { get; set; } = [];
+        public IEnumerable<BusinessAppointmentViewModel> Canceled { get; set; } = [];
+    }
+}
--- AppointMeWeb.Core/Models/HomeModels/UserHomeIndexView.cs
+++ AppointMeWeb.Core/Models/HomeModels/UserHomeIndexView.cs
@@ -11,5 +11,6 @@ namespace AppointMeWeb.Core.Models.HomeModels
         public IEnumerable<UserHomeIndexViewModel> Active { get; set; } = [];
         public IEnumerable<UserHomeIndexViewModel> Canceled { get; set; } = [];
 
+       
     }
 }
