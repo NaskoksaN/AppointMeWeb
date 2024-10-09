521a1dd576c1db916d201582b7e9417d9acca950
NaskoksaN <nbobchev@yahoo.com>
2024-10-09 20:51:29 +0300

    Create ViewModel DateRange

---
+++ AppointMeWeb.Core/Models/Schedule/DateRange.cs
@@ -0,0 +1,8 @@
+﻿namespace AppointMeWeb.Core.Models.Schedule
+{
+    public class DateRange
+    {
+        public DateOnly StartDate { get; set; } 
+        public DateOnly EndDate { get; set; }   
+    }
+}
