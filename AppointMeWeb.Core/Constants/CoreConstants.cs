28d4882add280edf1cdf1b01e1f38771e01ab032
NaskoksaN <nbobchev@yahoo.com>
2024-10-09 20:52:02 +0300

    add regex for mail in Constants

--- AppointMeWeb.Core/Constants/CoreConstants.cs
+++ AppointMeWeb.Core/Constants/CoreConstants.cs
@@ -8,6 +8,7 @@ namespace AppointMeWeb.Core.Constants
 {
     public static class CoreConstants
     {
-        public  const int CountOfDays= 30;
+        public const int CountOfDays = 30;
+        public const string RegexEmailValidation = @"^[^@\s]+@[^@\s]+\.[a-zA-Z]{2,}$";
     }
 }
