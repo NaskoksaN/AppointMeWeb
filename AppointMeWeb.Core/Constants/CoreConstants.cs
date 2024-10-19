da0ebff690f0a09b54a55233112bcf1d9e6e68e3
NaskoksaN <nbobchev@yahoo.com>
2024-10-19 14:04:01 +0300

    Add constatnt

--- AppointMeWeb.Core/Constants/CoreConstants.cs
+++ AppointMeWeb.Core/Constants/CoreConstants.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppointMeWeb.Core.Constants
+﻿namespace AppointMeWeb.Core.Constants
 {
     public static class CoreConstants
     {
         public const int CountOfDays = 30;
         public const string RegexEmailValidation = @"^[^@\s]+@[^@\s]+\.[a-zA-Z]{2,}$";
         public const int MinAgeUser = 16;
+        public const int MaxDaySchedulePeriod = 30;
     }
 }
