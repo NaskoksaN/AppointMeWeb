7e2941f5dced5721bc1ff52099374bbbf2d0df02
NaskoksaN <nbobchev@yahoo.com>
2024-10-19 16:11:41 +0300

    Creat attribute, to check if enttime is less of start time. use it in DailyScheduleViewModel

---
+++ AppointMeWeb.Core/CustomValidations/EndTimeAfterStartTimeAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static AppointMeWeb.Core.Constants.MessageConstants;
+
+namespace AppointMeWeb.Core.CustomValidations
+{
+    public class EndTimeAfterStartTimeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                TimeSpan endTime = (TimeSpan)value;
+                var startTimeProperty = validationContext.ObjectType.GetProperty("StartTime");
+
+                if (startTimeProperty == null)
+                {
+                    return new ValidationResult(StartDateNotFoundErrMsg);
+                }
+
+                TimeSpan startTime = (TimeSpan)startTimeProperty.GetValue(validationContext.ObjectInstance, null);
+
+                //var durationProperty = validationContext.ObjectType.GetProperty("AppointmentDuration");
+
+                //if (durationProperty == null)
+                //{
+                //    return new ValidationResult(StartDateNotFoundErrMsg);
+                //}
+
+                //TimeSpan durationTime = (TimeSpan)durationProperty.GetValue(validationContext.ObjectInstance, null);
+
+                if (endTime  <= startTime)
+                {
+                    return new ValidationResult(EndTimeBeforeStartTimeErrMsg);
+                }
+            }
+                return ValidationResult.Success;
+        }
+    }
+}
