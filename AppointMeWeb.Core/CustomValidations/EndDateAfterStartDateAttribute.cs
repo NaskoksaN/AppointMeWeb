8d85630057beb18c72143a07012ecc2e6382d349
NaskoksaN <nbobchev@yahoo.com>
2024-10-19 14:47:27 +0300

    Remove constructor in Attribute. and refactor in ViewModel

--- AppointMeWeb.Core/CustomValidations/EndDateAfterStartDateAttribute.cs
+++ AppointMeWeb.Core/CustomValidations/EndDateAfterStartDateAttribute.cs
@@ -6,13 +6,6 @@ namespace AppointMeWeb.Core.CustomValidations
 {
     public class EndDateAfterStartDateAttribute : ValidationAttribute
     {
-        private readonly int _maxDaysDifference;
-
-        public EndDateAfterStartDateAttribute(int maxDaysDifference)
-        {
-            _maxDaysDifference = maxDaysDifference;
-        }
-
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var endDate = (DateOnly)value;
