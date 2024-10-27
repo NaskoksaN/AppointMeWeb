a9897cd0ecac9d2dc2e412f953654098b7302633
NaskoksaN <nbobchev@yahoo.com>
2024-10-27 17:23:26 +0200

    add constant fpr 7 days period

--- AppointMeWeb.Core/Constants/DateConstants.cs
+++ AppointMeWeb.Core/Constants/DateConstants.cs
@@ -6,6 +6,7 @@
         public static readonly DateOnly Tomorrow = DateOnly.FromDateTime(DateTime.Now.AddDays(1));
         public static readonly DateOnly NextWeekEnd = DateOnly.FromDateTime(DateTime.Now.AddDays(7));
         public static readonly DateOnly NextThirtyDays = Tomorrow.AddDays(30);
+        public static int RatePeriod = 7;
 
     }
 }
