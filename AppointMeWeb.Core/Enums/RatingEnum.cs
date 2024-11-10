274d518523efaece243d2df9e9123eca4719ffe8
NaskoksaN <nbobchev@yahoo.com>
2024-11-10 11:16:44 +0200

    Add RatingEnum for which allow user to give rating for used service

---
+++ AppointMeWeb.Core/Enums/RatingEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointMeWeb.Core.Enums
+{
+    public enum RatingEnum
+    {
+        Poor = 1,
+        Fair = 2,
+        Good = 3,
+        VeryGood = 4,
+        Excellent = 5
+    }
+}
