de91b2737425a9d0342131484b3bf575945b5e92
NaskoksaN <nbobchev@yahoo.com>
2024-11-10 09:03:40 +0200

    Create configuration folder, and add AppointmentConfiguration and RatingConfigration classes.

---
+++ AppointMeWeb.Infrastrucure/Data/Configuration/AppointmentConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using AppointMeWeb.Infrastrucure.Data.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AppointMeWeb.Infrastrucure.Data.Configuration
+{
+    public class AppointmentConfiguration : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+            builder.HasOne(u => u.ApplicationUser)
+                .WithMany(a => a.Appointments)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(u => u.BusinessServiceProvider)
+               .WithMany(a => a.Appointments)
+               .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
