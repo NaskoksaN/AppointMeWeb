ca4c5262e03dce38928d4173d02edb2dffb33e16
NaskoksaN <nbobchev@yahoo.com>
2024-11-10 09:04:21 +0200

    Create Table Rating in models. User can coment busiess provider services

---
+++ AppointMeWeb.Infrastrucure/Data/Models/Rating.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppointMeWeb.Infrastrucure.Data.Models
+{
+    [Comment("User business rating")]
+    public class Rating
+    {
+        [Key]
+        [Comment("Rating Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("User evaluation for service")]
+        public int Evaluation { get; set; }
+        [Required]
+        [Comment("User comment")]
+        public string Comment {  get; set; }=string.Empty;
+
+        [Required]
+        public string ApplicationUserId { get; set; } = string.Empty;
+        [Required]
+        [ForeignKey(nameof(ApplicationUserId))]
+        public ApplicationUser ApplicationUser { get; set; } = null!;
+
+        [Required]
+        public int BusinessId {  get; set; }
+        [Required]
+        [ForeignKey(nameof(BusinessId))]
+        public BusinessServiceProvider BusinessServiceProvider { get; set; } = null!;
+    }
+}
