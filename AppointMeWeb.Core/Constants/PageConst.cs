4c331456e767961bc362e35a84e3b5398ea88b94
NaskoksaN <nbobchev@yahoo.com>
2024-11-18 19:03:31 +0200

    Change from 6 to 8 cards per page

--- AppointMeWeb.Core/Constants/PageConst.cs
+++ AppointMeWeb.Core/Constants/PageConst.cs
@@ -2,7 +2,7 @@
 {
     public static class PageConst
     {
-        public const int BusinessPerPage = 6;
+        public const int BusinessPerPage = 8;
         public const int StartPage = 1;
     }
 }
